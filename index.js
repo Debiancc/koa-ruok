ae552a8fa1176d3ca11a430f73f7593c8887d328
debiancc <never.be.evil.debian@gmail.com>
2017-07-19 02:14:53 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const defaultRes = { result : true };
+
+const handleResponse = (p, context) => {
+  return Promise.resolve()
+                .then(() => {
+                  return typeof p === 'function' && p();
+                })
+                .then(r => {
+                  context.body = r || defaultRes;
+                })
+                .catch(err => {
+                  console.error(new Error(err));
+                  context.body = defaultRes;
+                });
+};
+
+/**
+ *
+ * @param path {String}
+ * @param res {*}
+ * @returns {Function}
+ */
+module.exports = function (path = '/ruok', res = defaultRes) {
+  return function (context, next) {
+    return path === context.path ? handleResponse(res, context) : next();
+  };
+};
